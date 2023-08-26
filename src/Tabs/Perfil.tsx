95a520ebda0067630493538e093ab415c8cf760b
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-08-26 04:50:38 -0300

    Feat: Tela de Perfil

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,11 +1,33 @@
-import { Text, VStack } from "native-base";
+import { Text, VStack, ScrollView, Avatar, Divider } from "native-base";
+import { Titulo } from "../components/Titulo";
 
 export default function Perfil() {
   return (
-    <VStack>
-      <Text>
-        Perfil
-      </Text>
-    </VStack>
+    <ScrollView flex={1}>
+      <VStack
+        flex={1}
+        alignItems="center"
+        p={5}
+      >
+        <Titulo color="blue.500">Meu Perfil</Titulo>
+
+        <Avatar 
+          size="xl"
+          source={{ uri: "https://images.pexels.com/photos/17991457/pexels-photo-17991457/free-photo-of-preto-negro-cropped-top-cropped.jpeg" }} 
+          mt={5}
+        />
+
+        <Titulo color="blue.500">Informações pessoais</Titulo>
+        <Titulo fontSize="lg" mb={1}>Armanda T. Clark</Titulo>
+        <Text>12/12/1998</Text>
+        <Text>El Sobrante, CA</Text>
+
+        <Divider mt={5} />
+
+        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
+        <Text>Bronquite</Text>
+        <Text>Sinusite</Text>
+      </VStack>
+    </ScrollView>
   );
 }
